2e58d3cdd1d4cbf6725056c33eda2832147af855
LWenK <m@lwenk.com>
2025-03-21 16:58:09 +0800

    fix: lastSigninTime

--- src/mod/SignIn/SignIn.cpp
+++ src/mod/SignIn/SignIn.cpp
@@ -72,7 +72,7 @@ void setLastTime(mce::UUID uuid) {
             "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}",
             tm.tm_year + 1900,
             tm.tm_mon + 1,
-            tm.tm_mday + 1,
+            tm.tm_mday,
             tm.tm_hour,
             tm.tm_min,
             tm.tm_sec
